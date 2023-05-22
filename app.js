85d7289419869a2ede372df33ed7c2517798d65e
Roy Anderson <royanderson.k@gmail.com>
2023-05-22 11:19:29 -0700

    throw error to ensure health check is working

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ app.listen(PORT, () => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('2') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.status(200).send('ok')
 })
